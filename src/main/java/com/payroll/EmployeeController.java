98119fafbf3f5bebb4c81ec17d0a4823863f4d37
Leo Alves <leonardo.filipe.alves@gmail.com>
2018-11-27 18:24:07 +0100

    make employee all use streams and resources

--- src/main/java/com/payroll/EmployeeController.java
+++ src/main/java/com/payroll/EmployeeController.java
@@ -1,8 +1,10 @@
 package com.payroll;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.Resources;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,9 +24,16 @@ class EmployeeController {
 	}
 
 	@GetMapping("/employees")
-	List<Employee> all() {
-		return repository.findAll();
-	}
+	Resources<Resource<Employee>> all() {
+
+		List<Resource<Employee>> employees = repository.findAll().stream()
+			.map(employee -> new Resource<>(employee,
+				linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
+				linkTo(methodOn(EmployeeController.class).all()).withRel("employees")))
+			.collect(Collectors.toList());
+
+		return new Resources<>(employees, linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
+}
 
 	@PostMapping("/employees")
 	Employee newEmployee(@RequestBody Employee newEmployee) {
