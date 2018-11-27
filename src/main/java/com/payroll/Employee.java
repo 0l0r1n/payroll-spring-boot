ff9c9d8c227c90e66557e6284509878e9bb9f7f0
Leo Alves <leonardo.filipe.alves@gmail.com>
2018-11-27 18:18:07 +0100

    nice progress

--- src/main/java/com/payroll/Employee.java
+++ src/main/java/com/payroll/Employee.java
@@ -1,21 +1,20 @@
 package com.payroll;
 
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Data
-@Getter @Setter
 @Entity
 class Employee {
 
 	private @Id @GeneratedValue Long id;
 	private String name;
 	private String role;
+
+	Employee() {}
 
 	Employee(String name, String role) {
 		this.name = name;
